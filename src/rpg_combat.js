15f487e8c1aa59d009ea3f7c5b38a8c50f7844a8
Arturo <arturomf88@gmail.com>
2022-01-18 19:45:54 +0100

    iteration one. Punto 2

--- src/rpg_combat.js
+++ src/rpg_combat.js
@@ -1,12 +1,19 @@
 'use strict'
 
-class Character {
+const Character = {
+    health: 1000,
+    level: 1,
+    alive: true,
+    damageReceived(value) {
+        const newHealth = Character.health - value;
+        if (newHealth <= 0) {
+            Character.health = 0;
+            Character.alive = false;
+            return Character.health;
+        }
+        return Character.health = newHealth;
+    },
 
-    constructor() {
-        this.health = 1000;
-        this.level = 1;
-        this.alive = true;
-    }
 }
 
 module.exports = Character
--- test/rpg_combat.test.js
+++ test/rpg_combat.test.js
@@ -1,13 +1,21 @@
 const Character = require("../src/rpg_combat");
 
+
 describe('Given a character', () => {
 
     test('when has been created, then it health must start at 1000, level must start at 1, and alive must start at true', () => {
-        let character = new Character();
-        expect(character.health).toBe(1000);
-        expect(character.level).toBe(1);
-        expect(character.alive).toBe(true)
+        // let character = new Character();
+        expect(Character.health).toBe(1000);
+        expect(Character.level).toBe(1);
+        expect(Character.alive).toBe(true)
     });
 
-
+    test('When damage received exceeds current Health, Health becomes 0 and the character dies', () => {
+        // let character = new Character();
+        expect(Character.health).toBe(1000);
+        expect(Character.alive).toBe(true);
+        Character.damageReceived(1001);
+        expect(Character.health).toEqual(0);
+        expect(Character.alive).toEqual(false);
+    })
 })
